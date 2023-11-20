e0819740c73df39a85f83144b932ff445e4c61ed
mSpiker1 <mspiker@embarqmail.com>
2023-11-19 22:05:42 -0500

    Add place pixel button to update database on press

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { db } from "./utils/firebase.js";
-import { onValue, ref, getDatabase, update, push } from "firebase/database";
+import { onValue, ref, getDatabase, update, push, get } from "firebase/database";
 import './App.css';
 
 function App() {
@@ -10,12 +10,11 @@ function App() {
   const [canvasSize, setCanvasSize] = useState({width: 512, height: 512});
   const [zoomLevel, setZoomLevel] = useState(1);
   const canvasRef = useRef(null);
+  const isMounted = useRef(true);
 
-  // Variables/Constants handling colors and color changes
+  // Constants handling colors and color changes
   const [selectedColor, setSelectedColor] = useState('#000000'); // Init with default to black
-
-  // Array of all 32 possible colors to use
-  const colors = [
+  const colors = [ // Array of all 32 colors available to users
     {name: 'burgandy', hex: '#6d001a'}, {name: 'darkred', hex: '#be0039'},
     {name: 'red', hex: '#ff4500'}, {name: 'orange', hex: '#ffa800'},
     {name: 'yellow', hex: '#ffd635'}, {name: 'cream', hex: '#fff8b8'},
@@ -33,6 +32,33 @@ function App() {
     {name: 'darkgrey', hex: '#515252'}, {name: 'grey', hex: '#898d90'},
     {name: 'lightgrey', hex: '#d4d7d9'}, {name: 'white', hex: '#ffffff'}
   ]
+  const [sendButtonColor, setSendButtonColor] = useState(colors[0].hex);
+
+  // Constants to handle changes to the canvas
+  const [pendingChanges, setPendingChanges] = useState({});
+
+
+
+  // Handle updating the canvas
+  const updateCanvas = useCallback((context, data) => {
+    context.clearRect(0, 0, canvasSize.width * zoomLevel, canvasSize.height * zoomLevel);
+
+    // Iterate through each grid in the canvas
+    Object.keys(data).forEach((gridKey) => {
+      const grid = data[gridKey];
+
+      // Iterate through each pixel within the given grid
+      Object.keys(grid).forEach((pixelKey) => {
+        const pixel = grid[pixelKey];
+        context.fillStyle = pixel.color;
+        context.fillRect(
+          pixel.x * zoomLevel + parseInt(gridKey.split('_')[0]) * 16 * zoomLevel,
+          pixel.y * zoomLevel + parseInt(gridKey.split('_')[1]) * 16 * zoomLevel,
+          zoomLevel, zoomLevel
+        );
+      });
+    });
+  }, [canvasSize, zoomLevel]);
 
 
 
@@ -43,37 +69,45 @@ function App() {
     const context = canvas.getContext('2d');
     const gridsRef = ref(db, 'canvas');
 
+    // logic to update the canvas on first go
+    const updateCanvas = (data) => {
+      // This adjusts canvas view size based on zoom level
+      canvas.width = canvasSize.width * zoomLevel;
+      canvas.height = canvasSize.height * zoomLevel;
+
+      context.fillStyle = '#FFFFFF';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Iterate through each grid in the canvas
+      Object.keys(grids).forEach((gridKey) => {
+      const grid = grids[gridKey];
+
+        // Iterate through each pixel within the given grid
+        Object.keys(grid).forEach((pixelKey) => {
+          const pixel = grid[pixelKey];
+          context.fillStyle = pixel.color;
+          context.fillRect(
+            pixel.x * zoomLevel + parseInt(gridKey.split('_')[0]) * 16 * zoomLevel,
+            pixel.y * zoomLevel + parseInt(gridKey.split('_')[1]) * 16 * zoomLevel,
+            zoomLevel, zoomLevel
+          );
+        });
+      });
+    };
+
     // Keep canvas pixels updated from database
     onValue(gridsRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
         setGrids(data);
+        updateCanvas(data);
       }
     });
-
-    // This adjusts canvas view size based on zoom level
-    canvas.width = canvasSize.width * zoomLevel;
-    canvas.height = canvasSize.height * zoomLevel;
-
-    context.fillStyle = '#FFFFFF';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Iterate through each grid in the canvas
-  Object.keys(grids).forEach((gridKey) => {
-    const grid = grids[gridKey];
-
-    // Iterate through each pixel within the given grid
-    Object.keys(grid).forEach((pixelKey) => {
-      const pixel = grid[pixelKey];
-      context.fillStyle = pixel.color;
-      context.fillRect(
-        pixel.x * zoomLevel + parseInt(gridKey.split('_')[0]) * 16 * zoomLevel,
-        pixel.y * zoomLevel + parseInt(gridKey.split('_')[1]) * 16 * zoomLevel,
-        zoomLevel, zoomLevel
-      );
-    });
-  });
-  }, [grids, canvasSize, zoomLevel]);
+    
+    return () => {
+      isMounted.current = false;
+    };
+  }, [updateCanvas]);
 
 
 
@@ -82,6 +116,8 @@ function App() {
     const canvas = canvasRef.current;
     const rect = canvas.getBoundingClientRect();
     const context = canvas.getContext('2d');
+
+    console.log('User clicked on canvas, handleCanvasClick called.');
 
     // Scale pixel coordinates based on user zoom level
     const x = Math.floor((event.clientX - rect.left) / zoomLevel);
@@ -95,22 +131,67 @@ function App() {
     const pixelX = x % 16;
     const pixelY = y % 16;
 
-    // Get the existing grid or create a new one
-    const updatedGrid = grids[`${gridX}_${gridY}`] || {};
+    // Construct the key for the pending changes
+    const key = `${gridX}_${gridY}`;
 
-    // Update the pixel color
-    updatedGrid[`${pixelX}_${pixelY}`] = { x: pixelX, y: pixelY, color: selectedColor };
-
-    // Update firebase with new grid
-    update(ref(db, `canvas/${gridX}_${gridY}`), updatedGrid);
+    // Update the pixel color in the local state
+    setPendingChanges((prev) => ({
+      ...prev,
+      [key]: {
+        ...prev[key],
+        [`${pixelX}_${pixelY}`]: { x: pixelX, y: pixelY, color: selectedColor },
+      },
+    }));
 
     // Draw the new pixel to the canvas
     context.fillStyle = selectedColor;
     context.fillRect(
-      pixelX * 10 * zoomLevel + gridX * 16 * 10 * zoomLevel,
-      pixelY * 10 * zoomLevel + gridY * 16 * 10 * zoomLevel,
-      10 * zoomLevel, 10 * zoomLevel
+      pixelX * zoomLevel + gridX * 16 * zoomLevel,
+      pixelY * zoomLevel + gridY * 16 * zoomLevel,
+      zoomLevel, zoomLevel
     );
+    console.log('handleCanvasClick completed execution.');
+  };
+
+
+  
+  // Handle sending updates to the database
+  const handleSendUpdates = async () => {
+    // Check for pending changes
+    if(Object.keys(pendingChanges).length === 0){
+      console.log('No pending changes to send.');
+      return;
+    }
+
+    // Update the database with the pending changes
+    const gridsRef = ref(db, 'canvas');
+    try {
+      // Fetch the existing data from the database
+      const snapshot = await get(gridsRef);
+      const existingData = snapshot.val() || {};
+  
+      // Merge the pending changes with the existing data
+      const newData = { ...existingData };
+
+      Object.keys(pendingChanges).forEach((gridKey) => {
+        // Ensure the grid exists in the merged data
+        newData[gridKey] = newData[gridKey] || {};
+
+        // Merge pixels in the current grid in the database
+        Object.keys(pendingChanges[gridKey]).forEach((pixelKey) => {
+          newData[gridKey][pixelKey] = pendingChanges[gridKey][pixelKey];
+        });
+      });
+  
+      // Update the database with the merged data
+      await update(gridsRef, newData);
+  
+      console.log('Pending changes sent to the database.');
+      // Clear local state once the changes are uploaded to the database
+      setPendingChanges({});
+    } catch (error) {
+      console.error('Error sending pending changes to the database:', error.message);
+    }
   };
 
 
@@ -137,6 +218,7 @@ function App() {
 
     // Update the selected color for the user globally
     setSelectedColor(color.hex);
+    setSendButtonColor(color.hex);
   }
 
 
@@ -148,12 +230,17 @@ function App() {
         {colors.map((color) => (
           <div
           key={color.name}
-          className={'colorButton ${color.name}'}
+          className={`colorButton ${color.name}`}
           data-color={color.name}
           style={{backgroundColor: color.hex}}
           onClick={() => handleColorChange(color)}
           ></div>
         ))}
+        <button className="sendButton" 
+        style={{ '--button-color': sendButtonColor }}
+        onClick={handleSendUpdates}>
+          Place Pixel
+        </button>
       </div>
     </div>
   );
